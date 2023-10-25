b1fc9eb5bd7795365e669c9900dcb265b11ecea7
alfonsinacortinasluetic <9001488@alumnos.ufv.es>
2023-10-25 20:23:42 +0200

    tensors homework

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ AUTHOR = __author__
 NAME = __name__
 
 setup(
-    name                    = NAME,
-    version                 = VERSION,
-    description             = 'Tensor module to perform simple calculations with tensors',
-    author                  = AUTHOR,
-    author_email            = 'alfonsinacortinasluetic@gmail.com',
-    license                 = 'MIT',
-    python_requires         = '>=3.9.5',
-    packages                = find_packages(),
-    include_package_data    = True,
-    package_data            = {'': ['resources/*.csv','resources/clusters/*.csv']},
-    install_requires        = [
+    name                    =NAME,
+    version                 =VERSION,
+    description             ='Tensor module to perform simple calculations with tensors',
+    author                  =AUTHOR,
+    author_email            ='alfonsinacortinasluetic@gmail.com',
+    license                 ='MIT',
+    python_requires         ='>=3.9.5',
+    packages                =find_packages(),
+    include_package_data    =True,
+    package_data            ={'': ['resources/*.csv','resources/clusters/*.csv']},
+    install_requires        =[
                                 'pandas',
                                 'numpy',
                                 'torch'
