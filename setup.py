1e1074b092467f0cdbcb37318960bb58c075165c
alfonsinacortinasluetic <9001488@alumnos.ufv.es>
2023-10-25 11:56:13 +0200

    tensors homework

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from TensorCalculator import __author__,__version__,__name__
+
+
+VERSION = __version__
+AUTHOR = __author__
+NAME = __name__
+
+setup(
+    name                    = NAME,
+    version                 = VERSION,
+    description             = 'Brief description of your package',
+    author                  = AUTHOR,
+    author_email            = 'pedro.anquela@ufv.es',
+    license                 = 'MIT',
+    python_requires         = '>=3.9.5',
+    packages                = find_packages(),
+    include_package_data    = True,
+    package_data            = {'': ['resources/*.csv','resources/clusters/*.csv']},
+    install_requires        = [
+                                'pandas',
+                                'numpy',
+                                'torch'
+                                ]
+     )
